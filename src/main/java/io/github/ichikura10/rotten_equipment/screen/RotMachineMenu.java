87c24e9b2748235d701ccf535deb812acccc63f3
ichikura10 <254886939+ichikura10@users.noreply.github.com>
2026-05-09 18:01:20 +0900

    change/quickMoveStackの実装方法 BluSunrize様のImmersiveEngineeringを参照しました。

--- src/main/java/io/github/ichikura10/rotten_equipment/screen/RotMachineMenu.java
+++ src/main/java/io/github/ichikura10/rotten_equipment/screen/RotMachineMenu.java
@@ -11,6 +11,10 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraftforge.common.capabilities.ForgeCapabilities;
 import net.minecraftforge.items.SlotItemHandler;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nonnull;
+import java.util.List;
 
 public class RotMachineMenu extends AbstractContainerMenu {
     public final RotMachineBlockEntity blockEntity;
@@ -51,55 +55,65 @@ public class RotMachineMenu extends AbstractContainerMenu {
         return maxProgress != 0 && progress != 0 ? progress * progressArrowSize / maxProgress : 0;
     }
 
-    // CREDIT GOES TO: diesieben07 | https://github.com/diesieben07/SevenCommons
-    // must assign a slot number to each of the slots used by the GUI.
-    // For this container, we can see both the tile inventory's slots as well as the player inventory slots and the hotbar.
-    // Each time we add a Slot to the container, it automatically increases the slotIndex, which means
-    //  0 - 8 = hotbar slots (which will map to the InventoryPlayer slot numbers 0 - 8)
-    //  9 - 35 = player inventory slots (which map to the InventoryPlayer slot numbers 9 - 35)
-    //  36 - 44 = TileInventory slots, which map to our TileEntity slot numbers 0 - 8)
-    private static final int HOTBAR_SLOT_COUNT = 9;
-    private static final int PLAYER_INVENTORY_ROW_COUNT = 3;
-    private static final int PLAYER_INVENTORY_COLUMN_COUNT = 9;
-    private static final int PLAYER_INVENTORY_SLOT_COUNT = PLAYER_INVENTORY_COLUMN_COUNT * PLAYER_INVENTORY_ROW_COUNT;
-    private static final int VANILLA_SLOT_COUNT = HOTBAR_SLOT_COUNT + PLAYER_INVENTORY_SLOT_COUNT;
-    private static final int VANILLA_FIRST_SLOT_INDEX = 0;
-    private static final int TE_INVENTORY_FIRST_SLOT_INDEX = VANILLA_FIRST_SLOT_INDEX + VANILLA_SLOT_COUNT;
 
-    // THIS YOU HAVE TO DEFINE!
-    private static final int TE_INVENTORY_SLOT_COUNT = 2;  // must be the number of slots you have!
+    private final int ownSlotCount = 2;
+    /*
+     * BluSunrize
+     * Copyright (c) 2023
+     *
+     * These codes, quickMoveStack, moveItemStackToWithMayPlace, moveItemStackToWithMayPlace, MoveItemsFunc is licensed under "Blu's License of Common Sense"
+     * https://github.com/BluSunrize/ImmersiveEngineering/blob/1.20.1/LICENSE
+     */
     @Override
-    public ItemStack quickMoveStack(Player playerIn, int pIndex) {
-        Slot sourceSlot = slots.get(pIndex);
-        if (sourceSlot == null || !sourceSlot.hasItem()) return ItemStack.EMPTY;  //EMPTY_ITEM
-        ItemStack sourceStack = sourceSlot.getItem();
-        ItemStack copyOfSourceStack = sourceStack.copy();
-
-        // Check if the slot clicked is one of the vanilla container slots
-        if (pIndex < VANILLA_FIRST_SLOT_INDEX + VANILLA_SLOT_COUNT) {
-            // This is a vanilla container slot so merge the stack into the tile inventory
-            if (!moveItemStackTo(sourceStack, TE_INVENTORY_FIRST_SLOT_INDEX, TE_INVENTORY_FIRST_SLOT_INDEX
-                    + TE_INVENTORY_SLOT_COUNT, false)) {
-                return ItemStack.EMPTY;  // EMPTY_ITEM
-            }
-        } else if (pIndex < TE_INVENTORY_FIRST_SLOT_INDEX + TE_INVENTORY_SLOT_COUNT) {
-            // This is a TE slot so merge the stack into the players inventory
-            if (!moveItemStackTo(sourceStack, VANILLA_FIRST_SLOT_INDEX, VANILLA_FIRST_SLOT_INDEX + VANILLA_SLOT_COUNT, false)) {
+    public @NotNull ItemStack quickMoveStack(Player player, int slot) {
+        ItemStack itemstack = ItemStack.EMPTY;
+        Slot slotObject = this.slots.get(slot);
+        if (slotObject.hasItem()) {
+            ItemStack itemstack1 = slotObject.getItem();
+            itemstack = itemstack1.copy();
+            if (slot < ownSlotCount) {
+                if (!this.moveItemStackTo(itemstack1, ownSlotCount, this.slots.size(), true))
+                    return ItemStack.EMPTY;
+            } else if (!this.moveItemStackToWithMayPlace(itemstack1, 0, ownSlotCount))
                 return ItemStack.EMPTY;
-            }
-        } else {
-            System.out.println("Invalid slotIndex:" + pIndex);
-            return ItemStack.EMPTY;
+
+            if (itemstack1.isEmpty())
+                slotObject.set(ItemStack.EMPTY);
+            else
+                slotObject.setChanged();
         }
-        // If stack size == 0 (the entire stack was moved) set slot contents to null
-        if (sourceStack.getCount() == 0) {
-            sourceSlot.set(ItemStack.EMPTY);
-        } else {
-            sourceSlot.setChanged();
-        }
-        sourceSlot.onTake(playerIn, sourceStack);
-        return copyOfSourceStack;
+
+        return itemstack;
     }
+
+    protected boolean moveItemStackToWithMayPlace(ItemStack pStack, int pStartIndex, int pEndIndex) {
+        return moveItemStackToWithMayPlace(slots, this::moveItemStackTo, pStack, pStartIndex, pEndIndex);
+    }
+
+    public static boolean moveItemStackToWithMayPlace(List<Slot> slots, MoveItemsFunc move, ItemStack pStack, int pStartIndex, int pEndIndex) {
+        boolean inAllowedRange = true;
+        int allowedStart = pStartIndex;
+        for (int i = pStartIndex; i < pEndIndex; i++) {
+            boolean mayplace = slots.get(i).mayPlace(pStack);
+            if (inAllowedRange && !mayplace) {
+                if (move.moveItemStackTo(pStack, allowedStart, i, false))
+                    return true;
+                inAllowedRange = false;
+            } else if (!inAllowedRange && mayplace) {
+                allowedStart = i;
+                inAllowedRange = true;
+            }
+        }
+        return inAllowedRange && move.moveItemStackTo(pStack, allowedStart, pEndIndex, false);
+    }
+
+    public interface MoveItemsFunc {
+        boolean moveItemStackTo(ItemStack var1, int var2, int var3, boolean var4);
+    }
+
+    /**
+     * ----
+     **/
 
     @Override
     public boolean stillValid(Player player) {
