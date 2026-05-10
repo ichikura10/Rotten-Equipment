87ec08bf0327ec345705817a4acf1f9ff8e36341
ichikura10 <254886939+ichikura10@users.noreply.github.com>
2026-05-10 16:20:34 +0900

    feat/rotten_itemのrecipeを追加

--- src/main/java/io/github/ichikura10/rotten_equipment/item/ModItems.java
+++ src/main/java/io/github/ichikura10/rotten_equipment/item/ModItems.java
@@ -5,9 +5,13 @@ import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, RottenEquipment.MOD_ID);
+
+    public static final RegistryObject<Item> ROTTEN_ITEM = ITEMS.register("rotten_item",
+            () -> new Item(new Item.Properties()));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
