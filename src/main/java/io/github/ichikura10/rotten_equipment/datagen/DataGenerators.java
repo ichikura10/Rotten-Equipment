2a2a21e468cd87376b41a68ed23b79fea361689b
ichikura10 <254886939+ichikura10@users.noreply.github.com>
2026-05-17 10:27:01 +0900

    fix/装備のアイテムとしてのテクスチャが登録できていない 着た状態のテクスチャは未実装

--- src/main/java/io/github/ichikura10/rotten_equipment/datagen/DataGenerators.java
+++ src/main/java/io/github/ichikura10/rotten_equipment/datagen/DataGenerators.java
@@ -24,5 +24,7 @@ public class DataGenerators {
         generator.addProvider(event.includeServer(), ModLootTableProvider.create(packOutput));
 
         generator.addProvider(event.includeClient(), new ModBlockStateProvider(packOutput, existingFileHelper));
+
+        generator.addProvider(event.includeClient(), new ModItemModelProvider(packOutput, existingFileHelper));
     }
 }
